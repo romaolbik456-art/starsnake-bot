fd3ef33ea7130c5a4553361779be7614f6b7f7cb
romaolbik456-art <romaolbik456@gmail.com>
2026-05-09 13:28:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from telegram.ext import (
 
 # ══════════════════════════════════════════════
 BOT_TOKEN = "8635694534:AAHxYWfNaUCpUUkcF9v60plWlD5b0Ol0HDc"
-GAME_URL  = "https://prismatic-salmiakki-2a9c2f.netlify.app"
+GAME_URL  = "https://harmonious-gumption-5dee7a.netlify.app"
 # ══════════════════════════════════════════════
 
 logging.basicConfig(level=logging.INFO)
